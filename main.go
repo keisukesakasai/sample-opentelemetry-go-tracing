0960f0dbbd122e0ccb2661bb11e745093e157dc8
keisukesakasai <k6s4i5.3rx@gmail.com>
2023-06-04 09:59:54 +0900

    :sparkles: 標準機能実装

--- main.go
+++ main.go
@@ -2,107 +2,97 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
-	"os"
 	"time"
 
+	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/jaeger"
-	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
-
 	"go.opentelemetry.io/otel/trace"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
+
+func initProvider() (func(context.Context) error, error) {
+	ctx := context.Background()
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String("TodoAPI"),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create resource: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+	conn, err := grpc.DialContext(ctx, "http://localhost:14268/api/traces",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create gRPC connection to collector: %w", err)
+	}
+	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create trace exporter: %w", err)
+	}
+
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	otel.SetTracerProvider(tracerProvider)
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	return tracerProvider.Shutdown, nil
+}
 
 var tracer trace.Tracer
 
 func main() {
-	_, cleanup, err := InitializeTracing("OCHaCafe sample")
+	ctx := context.Background()
+
+	shutdown, err := initProvider()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer cleanup()
-	tracer = otel.Tracer("OCHaCafe sample")
+	defer shutdown(ctx)
 
-	ctx := context.Background()
-	Operation1(ctx)
-	Operation2(ctx)
-	Operation3(ctx)
+	// router 設定
+	r := gin.Default()
+	r.Use(otelgin.Middleware("OCHaCafe"))
+	r.GET("/ochacafe", ochacafe1)
+
+	fmt.Println("server start...")
+	r.Run(":3000")
 }
 
-func Operation1(ctx context.Context) {
-	_, span := tracer.Start(ctx, "op1")
+func ochacafe1(c *gin.Context) {
+	ctx := c.Request.Context()
+	_, span := tracer.Start(ctx, "ochacafe1")
 	defer span.End()
 	time.Sleep(100 * time.Millisecond)
+	ochacafe2(ctx)
 }
 
-func Operation2(ctx context.Context) {
-	_, span := tracer.Start(ctx, "op2")
+func ochacafe2(ctx context.Context) {
+	_, span := tracer.Start(ctx, "ochacafe2")
 	defer span.End()
 	time.Sleep(100 * time.Millisecond)
+	ochacafe3(ctx)
 }
 
-func Operation3(ctx context.Context) {
-	_, span := tracer.Start(ctx, "op3")
+func ochacafe3(ctx context.Context) {
+	_, span := tracer.Start(ctx, "ochacafe3")
 	defer span.End()
 	time.Sleep(100 * time.Millisecond)
-}
-
-func InitializeTracing(serviceName string) (*sdktrace.TracerProvider, func(), error) {
-	exporter, err := NewJaegerExporter()
-	if err != nil {
-		return nil, nil, err
-	}
-
-	r := NewResource(serviceName, "1.0.0", "local")
-	tp := sdktrace.NewTracerProvider(
-		sdktrace.WithBatcher(exporter),
-		sdktrace.WithResource(r),
-		sdktrace.WithSampler(sdktrace.AlwaysSample()),
-	)
-
-	otel.SetTracerProvider(tp)
-
-	cleanup := func() {
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		if err := tp.ForceFlush(ctx); err != nil {
-			log.Print(err)
-		}
-		ctx2, cancel2 := context.WithTimeout(context.Background(), 5*time.Second)
-		if err := tp.Shutdown(ctx2); err != nil {
-			log.Print(err)
-		}
-		cancel()
-		cancel2()
-	}
-	return tp, cleanup, nil
-}
-
-func NewResource(serviceName string, version string, environment string) *resource.Resource {
-	return resource.NewWithAttributes(
-		semconv.SchemaURL,
-		semconv.ServiceNameKey.String(serviceName),
-		semconv.ServiceVersionKey.String(version),
-		attribute.String("environment", environment),
-	)
-}
-
-func NewJaegerExporter() (sdktrace.SpanExporter, error) {
-	endpoint := "localhost:14268/api/traces"
-
-	exporter, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(endpoint)))
-	if err != nil {
-		return nil, err
-	}
-	return exporter, nil
-}
-
-func NewStdoutExporter() (sdktrace.SpanExporter, error) {
-	return stdouttrace.New(
-		stdouttrace.WithPrettyPrint(),
-		stdouttrace.WithWriter(os.Stderr),
-	)
 }
